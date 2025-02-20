3c740328c46db27475586482504247c833cfcc69
maryam-mehr <bsef21m035@pucit.edu.pk>
2025-02-20 10:36:18 +0500

    Add files via upload

---
+++ controllers/CartController.cs
@@ -0,0 +1,124 @@
+﻿using FrothyFrolics_server_.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace FrothyFrolics_server_.Controllers
+{
+    //[Route("api/[controller]")]
+    public class CartController : Controller
+    {
+        private readonly ICartRepository _cartItemRepository;
+
+        public CartController(ICartRepository cartItemRepository)
+        {
+            _cartItemRepository = cartItemRepository;
+        }
+
+
+        //[HttpGet("getbyemail")]
+        public IActionResult GetByEmail([FromQuery] string emailId)
+        {
+            var cartItems = _cartItemRepository.GetByEmail(emailId);
+            
+
+            if (cartItems == null || !cartItems.Any())
+            {
+
+                return Json(new List<CartItem>());
+                
+            }
+
+            
+            return Json(cartItems);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var cartItem = _cartItemRepository.GetById(id);
+            if (cartItem == null)
+            {
+
+                return Json(new CartItem());
+            }
+            return View(cartItem);
+        }
+
+        [HttpPost]
+        public IActionResult AddCartItem([FromBody] CartItem cartItem)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {    
+                var existingCartItem = _cartItemRepository.GetByTitle(cartItem.title,cartItem.email);
+                if (existingCartItem != null)
+                {
+                    existingCartItem.quantity += 1;
+                    _cartItemRepository.Update(existingCartItem);
+                }
+                else
+                {  
+                    _cartItemRepository.Add(cartItem);
+                }
+            }
+                catch (Exception ex)
+                {
+                return StatusCode(500, "Internal server error");
+            }
+            return Ok(cartItem);
+        }
+
+        
+
+        [HttpPost]
+        public IActionResult Create(CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _cartItemRepository.Add(cartItem);
+            }
+            return View(cartItem);
+        }
+
+        [HttpPost]
+        public IActionResult updateCartItem([FromBody] CartItem cartItem)
+        {
+            _cartItemRepository.Update(cartItem);
+            return Ok(cartItem);
+        }
+
+        public IActionResult deleteCartItem([FromBody] CartItem cartItem)
+        {
+            _cartItemRepository.Delete(cartItem.title,cartItem.email);
+            return Ok(cartItem);
+        }
+
+        
+        public IActionResult deleteCartItemByEmail([FromBody] string email)
+        {
+            
+            if (string.IsNullOrEmpty(email))
+            {
+                return BadRequest("Email cannot be null or empty.");
+            }
+
+            try
+            {
+                _cartItemRepository.DeleteCartItemByEmail(email);
+                return Ok($"All cart items for email {email} have been deleted.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ controllers/FoodController.cs
@@ -0,0 +1,134 @@
+﻿using FrothyFrolics_server_.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace FrothyFrolics_server_.Controllers
+{
+    public class FoodController : Controller
+    {
+        private readonly IFoodRepository _foodRepository;
+
+        public FoodController(IFoodRepository foodRepository)
+        {
+            _foodRepository = foodRepository;
+        }
+
+        public IActionResult FetchAll()
+        {
+            
+            var foodItems = _foodRepository.GetAll();
+            var json = JsonSerializer.Serialize(foodItems);
+            
+            return Json(foodItems);
+        }
+        
+        public IActionResult GetByCategory([FromQuery] String category)
+        {
+            
+
+            
+            var foodItems = _foodRepository.GetByCategory(category);
+
+            if (foodItems == null || !foodItems.Any())
+            {
+                
+                return NotFound(); // Or return an empty JSON array
+            }
+
+            
+            return Json(foodItems);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var foodItem = _foodRepository.GetById(id);
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+            return View(foodItem);
+        }
+
+        [HttpPost]
+        public IActionResult AddFoodItems([FromBody] List<FoodItem> foodItems)
+        {
+            if (!ModelState.IsValid)
+            {
+                
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                foreach (var item in foodItems)
+                {
+
+                    _foodRepository.Add(item);
+                }
+                return Ok(foodItems);
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Create(FoodItem foodItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _foodRepository.Add(foodItem);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(foodItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, FoodItem foodItem)
+        {
+            if (id != foodItem.quantity)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _foodRepository.Update(foodItem);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(foodItem);
+        }
+
+        public IActionResult GetByTitle([FromQuery] string title)
+        {
+            Console.WriteLine($"---{title}---");
+            
+            var foodItem = _foodRepository.GetByTitle(title);
+            Console.WriteLine(foodItem.imageResId);
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+            
+            return Json(foodItem);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var foodItem = _foodRepository.GetById(id);
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+            _foodRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
