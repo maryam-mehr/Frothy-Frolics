ef8e5fc90171944ec9dadfc613b7c706908e1743
maryam-mehr <bsef21m035@pucit.edu.pk>
2025-02-20 10:28:32 +0500

    Add files via upload

---
+++ OrderController.cs
@@ -0,0 +1,92 @@
+﻿using FrothyFrolics_server_.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace FrothyFrolics_server_.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        //[HttpGet("getbyemail")]
+        public IActionResult GetByEmail([FromQuery] string emailId)
+        {
+            var orders = _orderRepository.GetByEmail(emailId);
+            if (orders == null || !orders.Any())
+            {
+                return NotFound();
+            }
+            return Json(orders);
+        }
+
+        //[HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var order = _orderRepository.GetById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Json(order);
+        }
+
+        //[HttpPost("addorder")]
+        public IActionResult AddOrder([FromBody] Order order)
+        {
+
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _orderRepository.Add(order);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+
+            return Ok(order);
+        }
+
+        //[HttpPut("updateorder")]
+        public IActionResult UpdateOrder([FromBody] Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _orderRepository.Update(order);
+            return Ok(order);
+        }
+
+        //[HttpDelete("deleteorder/{id}")]
+        [HttpPost]
+        public IActionResult DeleteOrderByEmail([FromBody] string email)
+        {
+            
+            try
+            {
+                _orderRepository.Delete(email);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+
+            return Ok(email);
+        }
+    }
+}
---
+++ UserController.cs
@@ -0,0 +1,99 @@
+﻿using FrothyFrolics_server_.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace FrothyFrolics_server_.Controllers
+{
+    
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            
+        }
+
+
+        
+
+        [HttpGet]
+        public IActionResult GetByEmail([FromQuery] string emailId)
+        {
+            
+            var user = _userRepository.GetByEmail(emailId);
+
+            return Json(user);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var user = _userRepository.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser([FromBody] User user)
+        {
+            if (user.loyaltyPoints>500)
+            {
+                user.loyaltyPoints = 500;
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _userRepository.Add(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromBody] User user)
+        {
+            if (user.loyaltyPoints > 500)
+            {
+                user.loyaltyPoints = 500;
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _userRepository.Update(user);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteUserByEmail([FromBody] String email)
+        {
+            
+            try
+            {
+                _userRepository.Delete(email);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+
+            return Ok(email);
+        }
+    }
+}
